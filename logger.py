576be99ecffedbf6b36c8adaa3d54b2afe1beacd
swati2411kumari-2005 <swati2411kumari@gmail.com>
2026-06-26 13:54:36 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,23 @@
+"""
+Logger Module
+Smart File Organizer Pro
+"""
 
+import logging
+import os
+
+# Create logs folder if it doesn't exist
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+logging.basicConfig(
+    filename="logs/organizer.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def log_message(message):
+    """
+    Save messages to log file.
+    """
+    logging.info(message)
