a235112472b0e9feae5bc7a5b70629286b0b78db
swati2411kumari-2005 <swati2411kumari@gmail.com>
2026-06-26 13:58:24 +0530

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,1 +1,26 @@
+"""
+CSV Reader Module
+Smart File Organizer Pro
+"""
 
+import csv
+
+
+def read_csv(file_path):
+    """
+    Read a CSV file and display its contents.
+    """
+    try:
+        with open(file_path, mode="r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+
+            print("\nCSV Data\n")
+
+            for row in reader:
+                print(row)
+
+    except FileNotFoundError:
+        print("CSV file not found.")
+
+    except Exception as e:
+        print(f"Error: {e}")
