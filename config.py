218ce109dbefe34372ae84ffc97fd943cbf40596
swati2411kumari-2005 <swati2411kumari@gmail.com>
2026-06-26 13:52:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,26 @@
+"""
+Configuration File
+Smart File Organizer Pro
+"""
 
+FILE_TYPES = {
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".png": "Images",
+    ".gif": "Images",
+
+    ".pdf": "Documents",
+    ".txt": "Documents",
+    ".doc": "Documents",
+    ".docx": "Documents",
+
+    ".csv": "CSV",
+    ".xlsx": "Excel",
+
+    ".mp3": "Music",
+
+    ".mp4": "Videos",
+
+    ".zip": "Archives",
+    ".rar": "Archives"
+}
